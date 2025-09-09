a59aba674e7003243f78fb50268997722d5094a9
yll halimi <halimiyll@gmail.com>
2025-09-09 11:34:27 +0200

    generics

--- src/app.ts
+++ src/app.ts
@@ -1,157 +1,63 @@
-type Admin = {
-  name: string;
-  priviledges: string[];
-};
+// const names: Array<string | number> = [1,2, "ckemi"];
 
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+// console.log(names);
 
-type ElevatedEmployee = Admin & Employee;
+// const promise: Promise<string>  = new Promise((resolve, reject) => {
+//   setTimeout(()=> {
+//     resolve(`this is done!`);
+//   },2000);
+// });
 
-const e1: ElevatedEmployee = {
-  name: "max",
-  priviledges: ["create-server"],
-  startDate: new Date(),
-};
+function merge<T extends object , U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
 
-type Combinable = string | number;
-type numeric = number | boolean;
-type Universal = Combinable & numeric;
+const mergeObj = merge({ name: "Max", hobbies : ['Sports'] }, {age: 30});
+//can't access directly properties
 
 
+interface Lengthy{
+  length: number;
+}
 
+function countAndDescribe<T extends Lengthy>(element : T): [T, string]{
+  let descpriptionText = 'Got no value.';
+  if(element.length === 1){
+    descpriptionText = 'Got 1 element';
+  }else if(element.length > 1 ){
+    descpriptionText = 'Got ' + element.length + ' elements';
 
-function add(a: number, b: number):number; 
-function add(a: string, b: string):string; 
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
   }
-  return a + b;
+  return [element,descpriptionText];
+
 }
 
-const result = add(1,5);
+console.log(countAndDescribe(['SPORTS', 'COOKING']));
 
 
-const fetchedUserData = {
-  id: "u1",
-  name: "Max",
-  job: { title: "CEO" }
-};
-
-console.log(fetchedUserData.job?.title ?? "No job title");
-// Output: "CEO"
-
-const fetchedUserData2 = {
-  id: "u2",
-  name: "Anna"
-};
-
-interface Job {
-  title: string;
-  description: string;
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
 }
 
-interface User {
-  id: string;
-  name: string;
-  job?: Job; // optional property
+console.log(getProperty({name:'max', age: 30}, 'age'));
+
+
+class DataStorage<T extends string | number | boolean>{
+  private data: T[] = [];
+  addItem(item: T){
+    this.data.push(item);
+  }
+  removeItem(item:T){
+    if(this.data.indexOf(item) === -1){
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems(){
+    return[...this.data];
+  }
 }
 
-const fetchedUserData3: User = {
-  id: "u1", 
-  name: "bond"
-};
+// " f  "
 
-// Use optional chaining + nullish coalescing
-const jobTitle = fetchedUserData3.job?.title ?? "No job title";
-
-console.log(jobTitle); // "No job title"
-
-
-// type UnknownEmployee = Employee | Admin;
-// function printEmpoloyeeInformation(emp: UnknownEmployee) {
-//   console.log("Name: " + emp.name);
-//   if ("priviledges" in emp) {
-//     console.log("Priviledges: " + emp.priviledges);
-//   }
-//   if ("startDate" in emp) {
-//     console.log("Date: " + emp.startDate);
-//   }
-// }
-
-// printEmpoloyeeInformation(e1);
-
-// class Car {
-//   drive() {
-//     console.log("driving...");
-//   }
-// }
-// class Truck {
-//   drive() {
-//     console.log("driving a truck...");
-//   }
-//   loadCargo(ammount: number) {
-//     console.log("loading cargo ..." + ammount);
-//   }
-// }
-
-// type Vehicle = Car | Truck;
-
-// const v1 = new Car();
-// const v2 = new Truck();
-
-// function useVehicle(vehicle: Vehicle) {
-//   if (vehicle instanceof Truck) {
-//     vehicle.loadCargo(100);
-//   }
-
-//   if (vehicle instanceof Car) {
-//     vehicle.drive();
-//   }
-// }
-// useVehicle(v1);
-// useVehicle(v2);
-
-// interface Bird {
-//   kind: "bird";
-//   flyingSpeed: number;
-// }
-// interface Horse {
-//   kind: "horse";
-//   runningSpeed: number;
-// }
-
-// type Animal = Bird | Horse;
-
-// function moveAnimal(animal: Animal) {
-//   let speed;
-//   switch (animal.kind) {
-//     case `bird`:
-//       speed = animal.flyingSpeed;
-//       break;
-//     case `horse`:
-//       speed = animal.runningSpeed;
-//       break;
-//   }
-//   console.log("moving with speed: " + speed);
-// }
-
-// moveAnimal ({kind:'bird', flyingSpeed:100});
-
-// // const userInput = <HTMLInputElement>document.getElementById('msg-output')!; 
-// const userInput = document.getElementById('msg-output')! as HTMLInputElement;
-// userInput.value = "hi there";
-
-
-// interface ErrorContainer{ // {email: 'not a valid email, username: 'must start with character'}
-//     [prop: string]: string
-// }
-
-// const errorBag : ErrorContainer = {};
-// const errorBag1 : ErrorContainer = {
-//     email: "now a valid email",
-//     username: "Must start with a capital character"
-// };
